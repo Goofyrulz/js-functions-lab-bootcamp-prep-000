f557ee122ecb5af402fd7f13c8f09a09c5060335
Christopher Anderson <andersonchris227@gmail.com>
2018-08-18 04:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 function happyHolidays() {
   return "Happy holidays!";
-};
+}
 function happyHolidaysTo(name) {
-  return 'Happy holidays, + $(name)!";
-};
+  return 'Happy holidays, + ${name}!';
+}
 
 
 
